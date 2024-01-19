1338381e4c49ab2699f9575aa45068442fb918c1
Mauro Kraus <krs.mauro@gmail.com>
2024-01-19 14:41:29 -0500

    initial commit with react and MUI as dependencies2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Stack } from "@mui/material";
+
+const App = () => (
+  <Stack>
+    <h1>Hello</h1>
+    <h2>World</h2>
+  </Stack>
+);
+
+export default App;
