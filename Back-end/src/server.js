e1d581f3a7f4cc8b152c2cb9d59420607ab79d74
Mauro Kraus <krs.mauro@gmail.com>
2024-01-26 14:22:37 -0500

    adds tunneling command

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/items", itemRoutes);
 
-app.listen(port, () =>
-  console.log(`App listening on port http://localhost:${port}`)
-);
+app.listen(port, () => {
+  console.log(`open tunnel with: ssh -R shappy:80:localhost:3000 serveo.net`);
+  console.log(`App listening on port http://localhost:${port}`);
+});
